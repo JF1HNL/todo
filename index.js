46eeebe5a915c39e8b75d0ad0b1a5109a19f5f5f
JF1HNL <1118040@ed.tus.ac.jp>
2018-07-07 16:19:25 +0900

    vuejs

--- index.js
+++ index.js
@@ -1,79 +1,26 @@
-const list = document.querySelector('#list');
-let sw_flag = true;
-let i = 0;
-
-class cl_item{
-    constructor(content,rm_flag){
-        this.ele = document.createElement('div');
-        this.but_ele = document.createElement('input');
-        this.content = content;
-        this.rm_flag = rm_flag;
-        this.ele.innerText = this.content;
-        this.ele.class = 'item';
-        this.create_button();
-    }
-    create_button(){
-        this.but_ele.class = 'button';
-        this.but_ele.type = 'button';
-        if(this.rm_flag){
-            this.but_ele.value = 'done!';
-        }else{
-            this.but_ele.value = 'undone!';
+let Field = new Vue({
+    el: '#list',
+    data: {
+        title : 'udon!',
+        list : [
+        ],
+        show : true
+    },
+    computed: {
+        unshow_list: function(){
+            return this.list.filter(item => item.f_done === false)
         }
-        this.but_ele.addEventListener('click',this.press_button());
-        this.ele.appendChild(this.but_ele);
-    }
-    press_button(){
-        return () => {
-            if(this.rm_flag === true){
-                this.ele.style.textDecoration = 'line-through';
-                this.but_ele.value = 'undone!';
-                this.rm_flag = false;
-                if(sw_flag === false){
-                    list.removeChild(this.ele);
-                }
+    },
+    methods : {
+        add : function(){
+            this.list.push({text:this.title,f_done:false});
+            this.title = '';
+        },
+        done : function(item){
+            if(item.f_done){
+                item.f_done = false
             }else{
-                this.ele.style.textDecoration = '';
-                this.rm_flag = true;
-                this.but_ele.value = 'done!'
-            }
-        }
-    }
-}
-
-
-let item = [];
-const field = document.querySelector('#field');
-field.addEventListener('change', () => {
-    i++;
-    item[i] = new cl_item(field.value,true);
-    list.appendChild(item[i].ele);
-    field.value = '';
-})
-
-const switching = document.querySelector('#switching')
-switching.addEventListener('click', () =>{
-    if(sw_flag){
-        sw_flag = false;
-        switching.value = 'show';
-        if(i > 0){
-            for(let j = 1; j <= i; j++){
-                if(!item[j].rm_flag){
-                    list.removeChild(item[j].ele);
-                }
-            }
-        }
-    }else{
-        sw_flag = true;
-        switching.value = 'unshow';
-        if(i > 0){
-            for(let j = 1; j <= i; j++){
-                if(item[j].rm_flag){
-                    list.removeChild(item[j].ele);
-                }
-            }
-            for(let j = 1; j <= i; j++){
-                list.appendChild(item[j].ele);
+                item.f_done = true
             }
         }
     }
